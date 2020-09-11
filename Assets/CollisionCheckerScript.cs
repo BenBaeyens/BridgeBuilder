6ccc1bf9a406ca033ad7fcfae2f0f5af08f0fba9
Kjip <kjipgamer@gmail.com>
2020-09-11 23:54:47 +0200

    Added check to avoid own collider

--- Assets/CollisionCheckerScript.cs
+++ Assets/CollisionCheckerScript.cs
@@ -19,7 +19,7 @@ public class CollisionCheckerScript : MonoBehaviour
         for (int i = 0; i < gameManager.colliders.Count; i++)
         {
 
-            if (GetComponent<Collider>().bounds.Intersects(gameManager.colliders[i].bounds))
+            if (GetComponent<Collider>().bounds.Intersects(gameManager.colliders[i].bounds) && gameManager.colliders[i] != GetComponent<Collider>())
             {
                 isInCollision = true;
                 break;
